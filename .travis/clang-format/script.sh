fe3f686962b06ce11e4c0c12d8acc159691b1446
Valentin Vanelslande <valentinvanelslandeacnl@gmail.com>
2018-10-16 23:42:08 -0500

    fix clang-format script

--- .travis/clang-format/script.sh
+++ .travis/clang-format/script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -ex
 
-if grep -nrI '\s$' src *.yml *.txt *.md Doxyfile .gitignore .gitmodules .travis* dist/*.desktop \
-                 dist/*.svg dist/*.xml; then
+if grep -nrI '\s$' src *.yml *.txt *.md .gitignore .gitmodules .travis*; then
     echo Trailing whitespace found, aborting
     exit 1
 fi
