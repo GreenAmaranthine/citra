dcf835f70f2bb95e846d700b34d5994eb5520cd3
Valentin Vanelslande <valentinvanelslandeacnl@gmail.com>
2018-10-17 00:27:12 -0500

    fixes

--- .travis/linux-frozen/docker.sh
+++ .travis/linux-frozen/docker.sh
@@ -3,5 +3,5 @@
 cd /citra
 
 mkdir build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DENABLE_DISCORD_RPC=ON -DENABLE_SCRIPTING=ON
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DENABLE_DISCORD_RPC=ON
 make -j4
