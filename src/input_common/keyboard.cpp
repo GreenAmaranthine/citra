f984c73f13429d8e2feafe5608c31a1e8f58e9d8
Valentin Vanelslande <valentinvanelslandeacnl@gmail.com>
2018-11-03 15:49:54 -0500

    fix typo

--- src/input_common/keyboard.cpp
+++ src/input_common/keyboard.cpp
@@ -1,7 +1,4 @@
-// C {
-    lock {
-        lock {
-            lock {tra Emulator Project
+// Copyright 2017 Citra Emulator Project
 // Licensed under GPLv2 or any later version
 // Refer to the license.txt file included.
 
