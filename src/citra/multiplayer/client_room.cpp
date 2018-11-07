11006031591a83a8ad65deb2fd7d74ca03857b5e
zhupengfei <zhupf321@gmail.com>
2018-11-07 22:41:56 +0800

    multiplayer: fix "Connected" message not appearing on first connection

--- src/citra/multiplayer/client_room.cpp
+++ src/citra/multiplayer/client_room.cpp
@@ -31,6 +31,8 @@ ClientRoomWindow::ClientRoomWindow(QWidget* parent, Core::System& system)
         [this](const Network::RoomMember::State& state) { emit StateChanged(state); });
     connect(this, &ClientRoomWindow::RoomInformationChanged, this, &ClientRoomWindow::OnRoomUpdate);
     connect(this, &ClientRoomWindow::StateChanged, this, &ClientRoomWindow::OnStateChange);
+    // Update the state
+    OnStateChange(member.GetState());
     connect(ui->disconnect, &QPushButton::released, [this] { Disconnect(); });
     ui->disconnect->setDefault(false);
     ui->disconnect->setAutoDefault(false);
