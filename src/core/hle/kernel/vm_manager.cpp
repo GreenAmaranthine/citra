278fc01953df999046586f5b2f00ac43e3077788
Valentin Vanelslande <valentinvanelslandeacnl@gmail.com>
2018-11-16 01:13:13 -0500

    fixes

--- src/core/hle/kernel/vm_manager.cpp
+++ src/core/hle/kernel/vm_manager.cpp
@@ -20,15 +20,13 @@ static const char* GetMemoryStateName(MemoryState state) {
     };
     return names[(int)state];
 }
+
 bool VirtualMemoryArea::CanBeMergedWith(const VirtualMemoryArea& next) const {
     ASSERT(base + size == next.base);
     if (permissions != next.permissions || meminfo_state != next.meminfo_state ||
         type != next.type) {
         return false;
     }
-    if (type == VMAType::AllocatedMemoryBlock &&
-        (backing_block != next.backing_block || offset + size != next.offset))
-        return false;
     if (type == VMAType::BackingMemory && backing_memory + size != next.backing_memory)
         return false;
     if (type == VMAType::MMIO && paddr + size != next.paddr)
@@ -60,25 +58,6 @@ VMManager::VMAHandle VMManager::FindVMA(VAddr target) const {
         return vma_map.end();
     else
         return std::prev(vma_map.upper_bound(target));
-}
-
-ResultVal<VMManager::VMAHandle> VMManager::MapMemoryBlock(VAddr target,
-                                                          std::shared_ptr<std::vector<u8>> block,
-                                                          std::size_t offset, u32 size,
-                                                          MemoryState state) {
-    ASSERT(block);
-    ASSERT(offset + size <= block->size());
-    // This is the appropriately sized VMA that will turn into our allocation.
-    CASCADE_RESULT(VMAIter vma_handle, CarveVMA(target, size));
-    VirtualMemoryArea& final_vma{vma_handle->second};
-    ASSERT(final_vma.size == size);
-    final_vma.type = VMAType::AllocatedMemoryBlock;
-    final_vma.permissions = VMAPermission::ReadWrite;
-    final_vma.meminfo_state = state;
-    final_vma.backing_block = block;
-    final_vma.offset = offset;
-    UpdatePageTableForVMA(final_vma);
-    return MakeResult<VMAHandle>(MergeAdjacent(vma_handle));
 }
 
 ResultVal<VAddr> VMManager::MapBackingMemoryToBase(VAddr base, u32 region_size, u8* memory,
@@ -163,8 +142,6 @@ VMManager::VMAIter VMManager::Unmap(VMAIter vma_handle) {
     vma.type = VMAType::Free;
     vma.permissions = VMAPermission::None;
     vma.meminfo_state = MemoryState::Free;
-    vma.backing_block = nullptr;
-    vma.offset = 0;
     vma.backing_memory = nullptr;
     vma.paddr = 0;
     UpdatePageTableForVMA(vma);
@@ -200,16 +177,6 @@ ResultCode VMManager::ReprotectRange(VAddr target, u32 size, VMAPermission new_p
     while (vma != end && vma->second.base < target_end)
         vma = std::next(StripIterConstness(Reprotect(vma, new_perms)));
     return RESULT_SUCCESS;
-}
-
-void VMManager::RefreshMemoryBlockMappings(const std::vector<u8>* block) {
-    // If this ever proves to have a noticeable performance impact, allow users of the function to
-    // specify a specific range of addresses to limit the scan to.
-    for (const auto& p : vma_map) {
-        const VirtualMemoryArea& vma{p.second};
-        if (block == vma.backing_block.get())
-            UpdatePageTableForVMA(vma);
-    }
 }
 
 void VMManager::LogLayout(Log::Level log_level) const {
