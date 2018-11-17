2c14e3bbb57d7a8953f522a518870f6f09dd21cc
Weiyi Wang <wwylele@gmail.com>
2018-11-17 15:01:45 -0500

    Memory: remove ReadBlock with current process

--- src/core/hle/kernel/svc.cpp
+++ src/core/hle/kernel/svc.cpp
@@ -663,7 +663,7 @@ void SVC::OutputDebugString(VAddr address, s32 len) {
     if (len <= 0)
         return;
     std::string string(len, ' ');
-    Memory::ReadBlock(address, string.data(), len);
+    Memory::ReadBlock(*kernel.GetCurrentProcess(), address, string.data(), len);
     LOG_DEBUG(Debug_Emulated, "{}", string);
 }
 
