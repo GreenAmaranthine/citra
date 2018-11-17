1e11d7f2751ead35a21cd6ed18b302542c231102
Weiyi Wang <wwylele@gmail.com>
2018-11-17 15:06:56 -0500

    Memory: remove WriteBlock with current process

--- src/core/rpc/rpc_server.cpp
+++ src/core/rpc/rpc_server.cpp
@@ -24,15 +24,15 @@ RPCServer::~RPCServer() {
 
 void RPCServer::HandleReadMemory(Packet& packet, u32 address, u32 data_size) {
     // Note: Memory read occurs asynchronously from the state of the emulator
-    Memory::ReadBlock(*Core::System::GetInstance().Kernel().GetCurrentProcess(), address,
-                      packet.GetPacketData().data(), data_size);
+    Memory::ReadBlock(*system.Kernel().GetCurrentProcess(), address, packet.GetPacketData().data(),
+                      data_size);
     packet.SetPacketDataSize(data_size);
     packet.SendReply();
 }
 
 void RPCServer::HandleWriteMemory(Packet& packet, u32 address, const u8* data, u32 data_size) {
     // Note: Memory write occurs asynchronously from the state of the emulator
-    Memory::WriteBlock(address, data, data_size);
+    Memory::WriteBlock(*system.Kernel().GetCurrentProcess(), address, data, data_size);
     // If the memory happens to be executable code, make sure the changes become visible
     system.CPU().InvalidateCacheRange(address, data_size);
     packet.SetPacketDataSize(0);
