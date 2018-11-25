c9b91e7bb5d78607a9c887eac4d96fac7053306b
Valentin Vanelslande <valentinvanelslandeacnl@gmail.com>
2018-11-25 13:34:31 -0500

    dedicated_room: fix network version display

--- src/dedicated_room/citra-room.cpp
+++ src/dedicated_room/citra-room.cpp
@@ -48,7 +48,7 @@ static void PrintHelp(const char* argv0) {
 
 static void PrintVersion() {
     std::cout << "Citra dedicated room " << Common::g_scm_branch << " " << Common::g_scm_desc
-              << " Libnetwork: " << Network::NetworkVersion << std::endl;
+              << " Libnetwork: " << Network::NetworkVersion - 0xFF00 << std::endl;
 }
 
 static Network::Room::BanList LoadBanList(const std::string& path) {
